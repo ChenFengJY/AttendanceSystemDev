f449d6745da353066f7d7cafff37f231ba318ceb
zjty5418 <zjty5418@163.com>
2016-10-02 15:01:22 +0800

    添加BLL中转层

---
+++ BLL/AddSQLStringToDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+
+namespace BLL
+{
+    /// <summary>
+    /// 中转层，用于执行DAL层的操作的数据库代码
+    /// </summary>
+    public class AddSQLStringToDAL
+    {
+        /// <summary>
+        /// 执行DAL层查询语句，并返回dataTable
+        /// </summary>
+        /// <param name="strSQl">要执行的Select语句</param>
+        /// <returns>查询回来的数据</returns>
+        public static DataTable GetDtBySQL(string strSQl)
+        {
+            return ConnHelper.GetDataTable(strSQl);
+        }
+    }
+}
