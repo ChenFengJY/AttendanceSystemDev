e99284b9064e7fc89e470693fe75210f8b1201b3
zjty5418 <zjty5418@163.com>
2016-10-06 14:37:10 +0800

    添加领导菜单

--- UI/AdminMasterPage.master.cs
+++ UI/AdminMasterPage.master.cs
@@ -15,44 +15,7 @@ public partial class MasterPage : System.Web.UI.MasterPage
 
     protected void TreeView1_TreeNodePopulate1(object sender, TreeNodeEventArgs e)
     {
-        AddSQLStringToDAL.FillTreeVMenu(e);
+        AddSQLStringToDAL.FillTreeVMenu(e, "Admin_Menu");
     }
 
-    private void FillChildNode(TreeNode node)
-    {
-        //SqlCommand sqlQuery = new SqlCommand();
-        //sqlQuery.CommandText
-
-        string sqlQuery = "Select * From Admin_Menu " +
-        " Where Parent_Node = '" + node.Text + "'";
-        DataTable dt = AddSQLStringToDAL.GetDtBySQL(sqlQuery);
-        if (dt.Rows.Count > 0)
-        {
-            foreach (DataRow row in dt.Rows)
-            {
-                TreeNode NewNode = new
-                    TreeNode(row["Child_Node"].ToString(), "", "", row["Navigate_Url"].ToString(), "");
-                NewNode.PopulateOnDemand = false;
-                node.ChildNodes.Add(NewNode);
-            }
-        }
-    }
-
-    private void FillParentNode(TreeNode node)
-    {
-        string sqlQuery = "Select DISTINCT Parent_Node From Admin_Menu";
-        DataTable dt = AddSQLStringToDAL.GetDtBySQL(sqlQuery);
-
-        if (dt.Rows.Count > 0)
-        {
-            foreach (DataRow row in dt.Rows)
-            {
-                TreeNode NewNode = new
-                    TreeNode(row["Parent_Node"].ToString(), "ddad");
-                NewNode.PopulateOnDemand = true;
-                NewNode.SelectAction = TreeNodeSelectAction.Expand;
-                node.ChildNodes.Add(NewNode);
-            }
-        }
-    }
 }
---
+++ UI/LeaderMasterPage.master.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using BLL;
+public partial class LeaderMasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TreeView1_TreeNodePopulate1(object sender, TreeNodeEventArgs e)
+    {
+        AddSQLStringToDAL.FillTreeVMenu(e, "Leader_Menu");
+    }
+
+}
