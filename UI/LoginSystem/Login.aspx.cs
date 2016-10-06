160a1eb0b5bc7a576c042808667194c3f84298d4
zjty5418 <zjty5418@163.com>
2016-10-06 21:15:29 +0800

    添加登录界面

--- UI/LoginSystem/Login.aspx.cs
+++ UI/LoginSystem/Login.aspx.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using BLL;
-using System.Data;
 
 public partial class LoginSystem_Login : System.Web.UI.Page
 {
@@ -16,14 +14,6 @@ public partial class LoginSystem_Login : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        string strSql = "select * from TabTeachers where UserID=" + TextBox1.Text + "";
-        DataTable dt = AddSQLStringToDAL.SetData(strSql);  
-        if (dt.Rows.Count == 1)
-        {
-            if (dt.Rows[0][3].ToString() == TextBox2.Text)
-            {
-                TextBox3.Text = "登录成功";
-            }
-        }
+
     }
 }
