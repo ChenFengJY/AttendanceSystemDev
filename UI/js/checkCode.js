a311ca36a66c166848226a5d4b973caa7ba118b9
zjty5418 <zjty5418@163.com>
2016-10-08 08:53:29 +0800

    update

--- UI/js/checkCode.js
+++ UI/js/checkCode.js
@@ -2,7 +2,7 @@
 window.onload = createCode;
 function createCode() {
     code = "";
-    var codeLength = 6;//验证码的长度      
+    var codeLength = 4;//验证码的长度      
     var checkCode = document.getElementById("checkCode");
     checkCode.value = "";
     var selectChar = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
