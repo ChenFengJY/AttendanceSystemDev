6c00bb9a7d473c83f67bf8d9a29966dee2568694
““zhujintao99” <1026341976@qq.com>
2016-10-22 23:20:33 +0800

    导入数据BLL层，添加AddNewTeacher

--- UI/Admin/LoadOtherTeacherInfo.aspx.cs
+++ UI/Admin/LoadOtherTeacherInfo.aspx.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Web.UI.HtmlControls;
+using BLL;
+using System.Web.UI.WebControls.WebParts;
 
 public partial class Admin_LoadOtherTeacherInfo : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(!IsPostBack){
+            if (Session["UserID"].ToString()=="")
+            {
+                Response.Redirect("//Default.aspx");
+            }
+            else {
+            }
+        }
 
     }
 }
