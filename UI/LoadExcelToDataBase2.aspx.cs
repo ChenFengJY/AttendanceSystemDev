26c38581167c84736343eb3aab6ad01dc592f35d
homewyj <homewyj@gmail.com>
2016-10-23 16:24:59 +0800

    UI调整

--- UI/LoadExcelToDataBase2.aspx.cs
+++ UI/LoadExcelToDataBase2.aspx.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using BLL;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Collections.Generic;
+
+public partial class LoadExcelToDataBase2 : System.Web.UI.Page
+{
+
+}
