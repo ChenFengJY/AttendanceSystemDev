653bf66287982612476f1042f11f13f6dc3de0a1
““zhujintao99” <1026341976@qq.com>
2016-11-01 08:26:12 +0800

    LoadExcelToDatabase

--- UI/Admin/LoadExcelToDatabase.aspx.cs
+++ UI/Admin/LoadExcelToDatabase.aspx.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using BLL;
 public partial class Admin_LoadExcelToDatabase : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if(!IsPostBack)
+        {
+            if(Session["UserID"].ToString()=="")
+            {
+                Response.Redirect("~//Default.aspx");
+
+            }
+            else
+            {
+                
+            }
+        }
+
+    }
+    protected void BtnImportTeachers_Click(object sender,EventArgs e)
+    {
+        
 
     }
 }
